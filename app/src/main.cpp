7eb2f33a0c5c49baf3e7f6ead91ae36718a2318b
Serge Babayan <babayan.serge@gmail.com>
2024-07-22 17:57:43 +0300

    adjust template end cmake for the simplest version

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,7 +2,27 @@
 
 int main(int argc, char * argv[]) {
 
+    const int target_value = 54;
+	int current_value = 0;
+	bool not_win = true;
 
+	std::cout << "Enter your guess:" << std::endl;
+
+	do {
+		std::cin >> current_value;
+
+		if (current_value < target_value) {
+			std::cout << "less than target value" << std::endl;
+		}
+		else if (current_value > target_value) {
+			std::cout << "greater than target value" << std::endl;
+		}
+		else {
+			std::cout << "you win!" << std::endl;
+			break;
+		}
+
+	} while(true);
 
     return EXIT_SUCCESS;
 }
