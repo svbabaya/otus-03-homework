9e66c7648860e86f864ce861ca3deaa27e5436d9
Serge Babayan <babayan.serge@gmail.com>
2024-07-22 22:00:56 +0300

    Add random_value

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,15 +1,28 @@
+#include "../headers/random_value.h"
+
 #include <iostream>
 
 int main(int argc, char * argv[]) {
 
-    const int target_value = 54;
+    const int max_value = 100;
+    const int target_value = random_value(max_value);
 	int current_value = 0;
 	bool not_win = true;
 
-	std::cout << "Enter your guess:" << std::endl;
+    std::cout << "Target value: " << target_value << std::endl;
+
+/* Enter your name*/
+
+	printf("Enter your guess between 1 and %d:\n", max_value);
 
 	do {
-		std::cin >> current_value;
+        do {
+		    std::cin >> current_value;
+            if (current_value < 1 || current_value > max_value) {
+                printf("Attention, between 1 and %d:\n", max_value);
+            } else break;
+
+        } while(true);
 
 		if (current_value < target_value) {
 			std::cout << "less than target value" << std::endl;
---
+++ app/src/random_value.cpp
@@ -0,0 +1,12 @@
+//#include "random_value.h"
+
+#include <cstdlib>
+#include <ctime>
+
+int random_value(int max_value) {
+
+	std::srand(std::time(nullptr)); // use current time as seed for random generator
+
+	return std::rand() % max_value + 1;
+
+}
